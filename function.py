a0c0560cf0b42228187300d150bfc361b651d221
宋鹏 <15712847805@163.com>
2018-07-05 13:56:39 +0800

    文件移动，function

---
+++ function.py
@@ -0,0 +1,9 @@
+
+# 基本 function 定义
+def fun_print(temp):
+    print("hello : ",temp)
+    return
+
+def fun_plus(temp1,temp2):
+    return temp1 + temp2
+
---
+++ main.py
@@ -0,0 +1,8 @@
+
+# 导入整个文件
+#import function;
+#function.fun_print('第一个function!')
+
+# 只导入某个文件中的一个function from *** import
+from function import fun_plus
+print(fun_plus(20,3))
