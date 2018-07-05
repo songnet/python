40ac3788087a29961eea788f1c7e19df7c99b665
宋鹏 <15712847805@163.com>
2018-07-05 17:33:23 +0800

    面向对象，类的继承

--- function.py
+++ function.py
@@ -1,4 +1,6 @@
 
+import os
+
 # 基本 function 定义
 def fun_print(temp):
     print("hello : ",temp)
@@ -7,3 +9,41 @@ def fun_print(temp):
 def fun_plus(temp1,temp2):
     return temp1 + temp2
 
+# filename accessmode buffering
+def fun_openfile(filenname,mode):
+    #打开文件
+    file = open(filenname,mode)
+    print(file.name)
+    print(file.closed)
+    print(file.readline())
+    file.close()
+
+# 面向对象
+class Employee: # 类名
+    '员工基类' # 内置 __doc__
+    count = 0 # 外部属性
+    parentAtttr = "父类中的变量"
+    def __init__(self,name,salary): #构造函数、初始化
+        print("父类中的构造函数")
+        self.name = name
+        self.salary = salary
+        Employee.count += 1
+
+    def showCount(self):
+        print("employee的数量是：",Employee.count)
+
+    def displayEmp(self):
+        print("name:",self.name,"salary:",self.salary)
+
+    def parentMethod(self):
+        print("调用父类中的方法")
+
+# 类的继承
+class Person (Employee):
+    'Person类，继承Employee'
+
+    def __init__(self):
+        print("子类中的构造函数")
+
+    def childMethod(self):
+        print("调用子类中的方法")
--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 
 # 导入整个文件
-#import function;
-#function.fun_print('第一个function!')
+import function;
+function.fun_print('第一个function!')
 
 # 只导入某个文件中的一个function from *** import
-from function import fun_plus
-print(fun_plus(20,3))
+#from function import fun_plus
+#print(fun_plus(20,3))
+
+# 文件读写
+#function.fun_openfile("foo1111.txt","r")
+
+# 面向对象
+# 1.实例化对象
+emp1 = function.Employee("songnet",100)
+emp2 = function.Employee("zhangwt",2000)
+# 2.调用方法
+emp1.displayEmp()
+emp2.displayEmp()
+emp2.showCount()
+# 3.访问属性
+print(function.Employee.count)
+print(function.Employee.__doc__)
+# 可以增加属性
+emp1.age = 201111
+print(emp1.age)
+# 或者使用 setattr  hasattr getsttr delattr
+setattr(emp2,'age',111111111)
+print(emp2.age);
+
+# 子类调用父类中的方法、及其属性
+person = function.Person()
+person.childMethod()
+person.parentMethod()
+print(person.parentAtttr)
